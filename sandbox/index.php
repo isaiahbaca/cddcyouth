5a296f84a1f9193f11a293deaa997e43ee66c23e
Isaiah <isaiah@atpay.com>
2015-11-01 16:12:53 -0700

    initial

---
+++ sandbox/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+
+   <title>	</title>
+
+<!-- meta stuff -->   
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+
+
+<!-- stylesheets -->
+	<link rel="stylesheet" href="style.css?id=<?php echo rand(1,1000); ?>" type="text/css">
+
+</head>
+
+
+<body>
+<div id="top">
+<div class="wrap">
+
+<div id="headerA">
+</div>
+
+</div>
+</div>
+
+
+
+
+<div id="middle">
+<div class="wrap">
+
+<div id="headerB"></div>
+
+
+<div id="mainNav">
+</div>
+
+<div id="mainCont">
+
+foo
+</div>
+
+
+
+</div>
+</div>
+
+<div id="bottom"></div>
+
+
+</body>
+</html>
+
+
+
---
+++ test/test/contact.php
@@ -0,0 +1,63 @@
+  		
+  		<div class="bg_two_third last">
+  			<h1 class="page_title">Get in touch!</h1>
+  			<p>
+  				<span class="dropcap">M</span>aecenas vel mauris non ipsum consectetur euismod vitae vel eros. Aliquam variturpis. 
+  				Donec ultricies magna a mi condimentum fringilla. Vivamus at augue tortor, suscipit 
+  				molestie magna. Nulla scelerisque dolor ac metus vehicula ac pharetra odio pellentesque. 
+  			</p>
+  		</div> <!-- bg_two_third -->
+  		
+  		<div class="bg_two_third last">
+  			<div id="errors"> <!-- To display the errors form -->
+  				<div id="success" class="download_box">Thank you! Your request has been send, we'll be back soon.</div>
+  			</div> <!-- errors -->
+  			<form action="securemail.php"  method="post" id="contact_form" class="form_validate">
+  			<input type="hidden" value="contact.html" name="redirect" />
+  				<ul>
+  					<li class="two_col">
+  						<label>First name*</label>
+  						<input class="single_input" name="First name" type="text" required="required" />
+  					</li>
+  					<li class="two_col">
+  						<label>E-mail*</label>
+  						<input class="single_input" name="email" type="email" required="required" />
+  					</li>
+  					<li class="two_col">
+  						<label>Last name*</label>
+  						<input class="single_input" name="Last name" type="text" required="required" />
+  					</li>
+  					<li class="two_col">
+  						<label>Website URL</label>
+  						<input class="single_input" name="Website URL" type="text" />
+  					</li>
+  					<li class="two_col">
+  						<label>Telephone</label>
+  						<input class="single_input" name="Telephone" type="text" />
+  					</li>
+  					<li class="two_col">
+  						<label>Company</label>
+  						<input class="single_input" name="Company" type="text" />
+  					</li>
+  					<li class="one_col">
+  						<label>Interest</label>
+  						<input type="checkbox" name="interest" value="seo" />SEO
+  						<input type="checkbox" name="interest" value="marketing" />marketing
+  						<input type="checkbox" name="interest" value="webdev" />Web Development
+  						<input type="checkbox" name="interest" value="" id="other" />Other
+  						<input id="select_other" type="text" name="interest" />
+  					</li>
+  					<li class="one_col">
+  						<label>Subject</label>
+  						<input class="big_input" name="subject" type="text" />
+  					</li>
+  					<li class="one_col">
+  						<label>Message</label>
+  						<textarea name="Message" id="message_form" cols="30" rows="10" required="required"></textarea>
+  					</li>
+  					<li class="one_col"><input type="submit" class="submit_form" /></li>
+  				</ul>
+  			</form>
+  		</div> <!-- bg_two_third -->
+  		  		  		
+
